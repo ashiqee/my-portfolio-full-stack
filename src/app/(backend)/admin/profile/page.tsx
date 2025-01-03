9189a44f6908e3f9f49308ac8f88bfcbc2d204e1
ashqee <ashiq.buet73@gmail.com>
2025-01-03 23:13:45 +0600

    profile some section added

--- src/app/(backend)/admin/profile/page.tsx
+++ src/app/(backend)/admin/profile/page.tsx
@@ -1,3 +1,24 @@
 export default function ProfileSettings() {
-  return <div>My Profile</div>;
+  return <div>
+    
+    My Profile
+
+{/* skills  */}
+<section>
+Skills
+</section>
+
+
+{/* education section  */}
+<section>
+Education
+</section>
+
+
+{/* experience section  */}
+<section>
+Experience
+</section>
+
+  </div>;
 }
